35efbff04dafe6470e945b0959567558c4cb97b3
Jon Merrifield <jon@jmerrifield.com>
2014-05-26 13:26:42 -0700

    Enable concurrency changing

--- a.test.js
+++ a.test.js
@@ -5,6 +5,10 @@ var async = require('async')
 var EventEmitter = require('events').EventEmitter
 
 function testFun(data, callback) {
+  if (data === 'throwTestErr') {
+    callback(new Error('Error'))
+  }
+
   this.running[data] = function () {
     delete this.running[data]
     callback(null, data)
@@ -20,11 +24,11 @@ function collector() {
   })
 }
 
-it('runs things in parallel', function (done) {
+beforeEach(function () {
   var context = {running: {}}
   var output = collector()
 
-  function completeAndAssertItem(item) {
+  this.completeAndAssertItem = function (item) {
     return function (callback) {
       events.once('data', function (data) {
         assert.equal(data, item)
@@ -35,34 +39,110 @@ it('runs things in parallel', function (done) {
     }
   }
 
-  function checkRunningFuncs(dataItems) {
+  this.checkRunningFuncs = function (dataItems) {
     return function (callback) {
       assert.deepEqual(Object.keys(context.running), dataItems)
       callback()
     }
   }
 
-  function assertStreamEnd() {
+  this.assertStreamEnd = function () {
     return function (callback) {
-      stream.on('end', callback)
-    }
+      this.stream.on('end', callback)
+    }.bind(this)
   }
 
-  var stream = subject(testFun.bind(context), 2)
-  stream.pipe(output)
-  stream.write('a')
-  stream.write('b')
-  stream.write('c')
-  stream.end()
+  this.changeLimit = function (fun) {
+    return function (callback) {
+      this.stream.changeLimit(fun)
+      callback()
+    }.bind(this)
+  }
+
+  this.stream = subject(testFun.bind(context), 2)
+  this.stream.pipe(output)
+})
+
+it('runs things in parallel', function (done) {
+  this.stream.write('a')
+  this.stream.write('b')
+  this.stream.write('c')
+  this.stream.end()
 
   async.series([
-    checkRunningFuncs(['a', 'b']),
-    completeAndAssertItem('a'),
-    checkRunningFuncs(['b', 'c']),
-    completeAndAssertItem('b'),
-    checkRunningFuncs(['c']),
-    completeAndAssertItem('c'),
-    checkRunningFuncs([]),
-    assertStreamEnd()
+    this.checkRunningFuncs(['a', 'b']),
+    // Throw in a noop changeLimit to make sure nothing changes
+    this.changeLimit(function (val) { return val }),
+    this.checkRunningFuncs(['a', 'b']),
+    this.completeAndAssertItem('a'),
+    this.checkRunningFuncs(['b', 'c']),
+    this.completeAndAssertItem('b'),
+    this.checkRunningFuncs(['c']),
+    this.completeAndAssertItem('c'),
+    this.checkRunningFuncs([]),
+    this.assertStreamEnd()
   ], done)
+})
+
+it('can increase concurrency', function (done) {
+  this.stream.write('a')
+  this.stream.write('b')
+  this.stream.write('c')
+  this.stream.write('d')
+  this.stream.end()
+
+  function inc(amount, val) {
+    return val + amount
+  }
+
+  async.series([
+    this.checkRunningFuncs(['a', 'b']),
+    this.completeAndAssertItem('a'),
+    this.checkRunningFuncs(['b', 'c']),
+    this.changeLimit(inc.bind(null, 1)),
+    this.checkRunningFuncs(['b', 'c', 'd']),
+    this.completeAndAssertItem('b'),
+    this.checkRunningFuncs(['c', 'd']),
+    this.completeAndAssertItem('c'),
+    this.checkRunningFuncs(['d']),
+    this.completeAndAssertItem('d'),
+    this.checkRunningFuncs([]),
+    this.assertStreamEnd()
+  ], done)
+})
+
+it('can decrease concurrency', function (done) {
+  this.stream.write('a')
+  this.stream.write('b')
+  this.stream.write('c')
+  this.stream.write('d')
+  this.stream.end()
+
+  function dec(amount, val) {
+    return val - amount
+  }
+
+  async.series([
+    this.checkRunningFuncs(['a', 'b']),
+    this.completeAndAssertItem('a'),
+    this.checkRunningFuncs(['b', 'c']),
+    this.changeLimit(dec.bind(null, 1)),
+    this.checkRunningFuncs(['b', 'c']),
+    this.completeAndAssertItem('b'),
+    this.checkRunningFuncs(['c']),
+    this.completeAndAssertItem('c'),
+    this.checkRunningFuncs(['d']),
+    this.completeAndAssertItem('d'),
+    this.checkRunningFuncs([]),
+    this.assertStreamEnd()
+  ], done)
+})
+
+it('passes along errors', function (done) {
+  this.stream.on('error', function (err) {
+    assert(err instanceof Error)
+    done()
+  })
+
+  this.stream.write('throwTestErr')
 })
--- index.js
+++ index.js
@@ -3,26 +3,46 @@ var through = require('through2')
 module.exports = function (asyncFn, initialLimit) {
   var current = 0
   var cbCount = 0
+  var outstandingCallbacks = []
   var onFlush = function () { }
+  var limit = initialLimit
 
-  return through.obj(function (chunk, enc, callback) {
-    function maybeCb() {
-      if (cbCount > 0) {
-        cbCount--
-        callback()
-      }
+  function maybeCb(callback) {
+    if (current === limit) return
+    if (cbCount > 0) {
+      cbCount--
+      outstandingCallbacks.splice(outstandingCallbacks.indexOf(callback, 1))
+      callback()
     }
+  }
+  var stream = through.obj(function (chunk, enc, callback) {
+    outstandingCallbacks.push(callback)
 
     asyncFn(chunk, function (err, result) {
       current--
+      if (err) return this.emit('error', err)
       this.push(result)
-      maybeCb()
+      maybeCb(callback)
       if (current === 0) onFlush()
     }.bind(this))
 
     cbCount++
-    if (++current < initialLimit) maybeCb()
+    if (++current < limit) maybeCb(callback)
   }, function (callback) {
     onFlush = callback
   })
+
+  stream.changeLimit = function (modifier) {
+    var oldLimit = limit
+    limit = modifier(limit)
+
+    var diff = limit - oldLimit
+    var numNewItemsToAccept = Math.max(0, diff)
+
+    for (var i = 0; i < numNewItemsToAccept; i++) {
+      maybeCb(outstandingCallbacks[0])
+    }
+  }
+
+  return stream
 }
